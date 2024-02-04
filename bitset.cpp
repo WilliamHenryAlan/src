b69a3702c19a801de8f76279747617fdd0328131
alan <1413775276@qq.com>
2024-02-04 20:49:31 +0800

    origin

--- bitset.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <bitset>
-int main() {
-    char a = 0b10011100;
-    char b = 0b10001110;
-    char s = a+b;
-    std::bitset<8> out(s);
-    std::cout << out << std::endl;
-}
--- main.py
+++
@@ -1,4 +0,0 @@
-print('today is %sday %d %d'%('mon',10,27))
-print('today is {}day {}month {}day'.format('mon',10,27))
-print('today is {1}day {2}month {0}day'.format('mon',10,27))
-help(hash)
