d4be9a18a503ae5a7a1f330432b95928499aa00a
alan <1413775276@qq.com>
2024-02-11 17:54:10 +0800

    init

---
+++ cpp/demo/demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  demo
+//
+//  Created by Willian alan on 2024/2/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
